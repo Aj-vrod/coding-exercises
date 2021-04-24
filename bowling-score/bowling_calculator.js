cde3197a7e6b943344f4f9490de2044d0e00a8af
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-24 15:40:45 +0200

    removed unnecesary lines in bowling exercise

--- bowling-score/bowling_calculator.js
+++ bowling-score/bowling_calculator.js
@@ -7,30 +7,21 @@ const bowlingScore = (rolls) => {
   if (rolls.length > 0) {
     // iterate over array
     rolls.forEach((roll, index) => {
-      // console.log(finalScore);
-      // console.log(rollsCounter);
-      // console.log(roll);
       // 9 first rows: general rules
       if (roll === 'Skip') {
-        console.log('skipped');
       } else if (rollsCounter < 10) {
         // if first score is not 10, sum the following one
-        // console.log(rolls);
         if (roll < 10) {
           roll += rolls[index + 1];
           // if get fewer than 10 with two rolls, the sum is your score
           if (roll < 10) {
             finalScore += roll;
             rolls[index + 1] = 'Skip';
-            // rolls.splice(0, 2);
-            // console.log(rolls);
           } else if (roll === 10) {
             // if you get 10 with two (spare), score is this 10 plus next roll's pins
             roll += rolls[index + 2];
             finalScore += roll;
             rolls[index + 1] = 'Skip';
-            // rolls.splice(0, 2);
-            // console.log(rolls);
           }
         } else if (roll === 10) {
           // your score is this 10 plus pins from the two follosing rolls
@@ -39,7 +30,6 @@ const bowlingScore = (rolls) => {
           // get general score
           finalScore += roll;
           // if first is 10 (strike), the following is another roll (you don't get second roll)
-          // rolls.shift();
         }
         rollsCounter += 1;
       } else if (rollsCounter === 10) {
@@ -51,7 +41,6 @@ const bowlingScore = (rolls) => {
           finalScore += roll;
           rolls[index + 1] = 'Skip';
           rolls[index + 2] = 'Skip';
-          // rolls.splice(0, 3);
         } else if (roll !== 10) {
           // if two first scores sum up 10, sum following roll
           roll += rolls[index + 1];
@@ -60,12 +49,10 @@ const bowlingScore = (rolls) => {
             finalScore += roll;
             rolls[index + 1] = 'Skip';
             rolls[index + 2] = 'Skip';
-            // rolls.splice(0, 3);
           } else {
             // if two rolls dont sum up 10, does not get a third roll
             finalScore += roll;
             rolls[index + 1] = 'Skip';
-            // rolls.splice(0, 2);
           }
         }
         // get total score
@@ -75,4 +62,4 @@ const bowlingScore = (rolls) => {
   return finalScore;
 };
 
-module.exports = bowlingScore; // Do not remove this line.
+module.exports = bowlingScore; // To export function
